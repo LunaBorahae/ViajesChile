e5da48bcc46d0faf8e4bc494239b73a163b0e938
Carolina Carmona <linacristal11@gmail.com>
2024-02-02 14:55:30 -0300

    commit1

---
+++ assets/js/viajes.js
@@ -0,0 +1,19 @@
+document.getElementById("miIcono1").addEventListener("click", function() {
+    // Redireccionar a la nueva URL
+    window.location.href = "https://github.com/";
+});
+
+document.getElementById("miIcono2").addEventListener("click", function() {
+    // Redireccionar a la nueva URL
+    window.location.href = "https://www.linkedin.com/";
+});
+
+document.getElementById("miIcono3").addEventListener("click", function() {
+    // Redireccionar a la nueva URL
+    window.location.href = "https://twitter.com/home?lang=es";
+});
+
+document.getElementById("miIcono4").addEventListener("click", function() {
+    // Redireccionar a la nueva URL
+    window.location.href = "https://www.facebook.com/";
+});
